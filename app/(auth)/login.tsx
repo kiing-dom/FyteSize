64e724823a274d2140c6a9bb2ee176b486af14ff
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-25 07:25:28 +0100

    added route to register screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import { Link } from 'expo-router';
+import React, { useState } from 'react';
 
 import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import { TextInput, Button } from 'react-native-paper';
 
 
 const LoginScreen = () => {
+
+  const [form, setForm] = useState({
+    email: '',
+    username: '',
+    password: ''
+  })
+
   return (
     <SafeAreaView className='h-full bg-white'>
       <ScrollView style={{ height: "100%" }}>
-        <View className='flex-1 justify-center items-center'>
+        <View className='flex-1 justify-center items-center min-h-[84vh]'>
           <Image
             source={require("../../assets/images/adaptive-icon.png")}
-            className = 'w-[400px] h-[400px]'
+            className='w-[400px] h-[400px]'
             resizeMode='contain'
           />
           <TextInput
             className='w-[80%] -mt-8 mb-2'
             mode='outlined'
             label="Email/Username"
+            value={form.email || form.username}
             activeOutlineColor='black'
+            keyboardType='email-address'
           />
           <TextInput
             className='w-[80%] mb-4'
             mode='outlined'
             label="Password"
+            value={form.password}
             activeOutlineColor='black'
             secureTextEntry={true}
           />
@@ -34,14 +45,9 @@ const LoginScreen = () => {
           >
             Login
           </Button>
-          <Button
-            mode='outlined'
-            className='w-[80%] mb-2 bg-white'
-            textColor='black'
-            labelStyle={{ fontSize: 20 }}
-          >
-            Register
-          </Button>
+          <Text className='text-[16px]'>
+            Dont have an account? <Link href="/register" className='text-blue-500'>Register</Link>
+          </Text>
 
         </View>
       </ScrollView>
