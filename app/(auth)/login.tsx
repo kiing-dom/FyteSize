9184beae9779b539bc06670d18d8a2ead57c69d6
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-25 06:48:22 +0100

    basic styling for login page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,5 +1,7 @@
-import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import React from 'react';
+
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
+import { TextInput, Button } from 'react-native-paper';
 
 
 const LoginScreen = () => {
@@ -9,9 +11,38 @@ const LoginScreen = () => {
         <View className='flex-1 justify-center items-center'>
           <Image
             source={require("../../assets/images/adaptive-icon.png")}
-            className = 'w-[512px] h-[512px]'
+            className = 'w-[400px] h-[400px]'
             resizeMode='contain'
           />
+          <TextInput
+            className='w-[80%] -mt-8 mb-2'
+            mode='outlined'
+            label="Email/Username"
+            activeOutlineColor='black'
+          />
+          <TextInput
+            className='w-[80%] mb-4'
+            mode='outlined'
+            label="Password"
+            activeOutlineColor='black'
+            secureTextEntry={true}
+          />
+          <Button
+            mode='contained'
+            className='w-[80%] mb-2 bg-black'
+            labelStyle={{ fontSize: 20 }}
+          >
+            Login
+          </Button>
+          <Button
+            mode='outlined'
+            className='w-[80%] mb-2 bg-white'
+            textColor='black'
+            labelStyle={{ fontSize: 20 }}
+          >
+            Register
+          </Button>
+
         </View>
       </ScrollView>
     </SafeAreaView>
