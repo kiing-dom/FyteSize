5f7ff5bd7e08c81639da5fcc069af2ff0e3ea902
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-25 16:21:09 +0100

    installed zustand | created useRegistrationStore hook to use in implentation of multistep registration form

---
+++ hooks/auth/useRegistrationStore.ts
@@ -0,0 +1,65 @@
+import { create } from 'zustand';
+
+interface FormData {
+    // Step 1: Boxing Information
+    boxingLevel: string;
+    gender: string;
+    weightClass: {
+        [key: string]: number;
+    };
+
+    // Step 2: Personal Information
+    age: string;
+    height: string;
+    currentWeight: string;
+    location: string;
+
+    // Step 3: Goal Setting
+    weightGoal: string;
+    activityLevel: string;
+
+    // Step 4:: Daily Calorie Goal Calculations
+    bmr: number;
+    tdee: number;
+
+    //Step: 5 Registration
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+};
+
+interface RegistrationState {
+    currentStep: number;
+    formData: FormData;
+    setCurrentStep: (step: number) => void;
+    updateFormData: (data: Partial<FormData>) => void
+}
+
+const useRegistrationStore = create<RegistrationState>((set) => ({
+    currentStep: 1,
+    formData: {
+        boxingLevel: '',
+        gender: '',
+        weightClass: {},
+        age: '',
+        height: '',
+        currentWeight: '',
+        location: '',
+        weightGoal: '',
+        activityLevel: '',
+        bmr: 0,
+        tdee: 0,
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    },
+    setCurrentStep: (step) => set({ currentStep: step}),
+    updateFormData: (data) =>
+        set((state) => ({
+            formData: {...state.formData, ...data}
+        })),
+}));
+
+export default useRegistrationStore;
