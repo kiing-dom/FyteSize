8c6c79037262f4246fa3fe7a44e0072c27907ebf
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-07-17 06:53:50 +0100

    small fixes...

--- hooks/auth/useRegistrationStore.ts
+++ hooks/auth/useRegistrationStore.ts
@@ -1,65 +1,88 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface FormData {
-    // Step 1: Boxing Information
-    boxingLevel: string;
-    gender: string;
-    weightClass: Record<string, string> | null;
+  // Step 1: Boxing Information
+  boxingLevel: string;
+  gender: string;
+  weightClass: Record<string, string> | null;
 
-    // Step 2: Personal Information
-    dateOfBirth: Date | null;
-    age: number;
-    height: string;
-    currentWeight: string;
-    location: string;
+  // Step 2: Personal Information
+  dateOfBirth: Date | null;
+  age: number;
+  height: string;
+  currentWeight: string;
+  location: string;
 
-    // Step 3: Goal Setting
-    weightGoal: string;
-    activityLevel: string;
+  // Step 3: Goal Setting
+  weightGoal: string;
+  activityLevel: string;
 
-    // Step 4:: Daily Calorie Goal Calculations
-    bmr: number;
-    tdee: number;
+  // Step 4:: Daily Calorie Goal Calculations
+  bmr: number;
+  tdee: number;
 
-    //Step: 5 Registration
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-};
+  //Step: 5 Registration
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
 
 interface RegistrationState {
-    currentStep: number;
-    formData: FormData;
-    setCurrentStep: (step: number) => void;
-    updateFormData: (data: Partial<FormData>) => void
+  currentStep: number;
+  formData: FormData;
+  setCurrentStep: (step: number) => void;
+  updateFormData: (data: Partial<FormData>) => void;
+  clearFormData: () => void;
 }
 
 const useRegistrationStore = create<RegistrationState>((set) => ({
-    currentStep: 1,
-    formData: {
-        boxingLevel: '',
-        gender: '',
+  currentStep: 1,
+  formData: {
+    boxingLevel: "",
+    gender: "",
+    weightClass: {},
+    dateOfBirth: null,
+    age: 0,
+    height: "",
+    currentWeight: "",
+    location: "",
+    weightGoal: "",
+    activityLevel: "",
+    bmr: 0,
+    tdee: 0,
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  },
+  setCurrentStep: (step) => set({ currentStep: step }),
+  updateFormData: (data) =>
+    set((state) => ({
+      formData: { ...state.formData, ...data },
+    })),
+
+  clearFormData: () =>
+    set({
+      formData: {
+        boxingLevel: "",
+        gender: "",
         weightClass: {},
         dateOfBirth: null,
         age: 0,
         height: "",
         currentWeight: "",
-        location: '',
-        weightGoal: '',
-        activityLevel: '',
+        location: "",
+        weightGoal: "",
+        activityLevel: "",
         bmr: 0,
         tdee: 0,
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: ''
-    },
-    setCurrentStep: (step) => set({ currentStep: step}),
-    updateFormData: (data) =>
-        set((state) => ({
-            formData: {...state.formData, ...data}
-        })),
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      },
+    }),
 }));
 
 export default useRegistrationStore;
